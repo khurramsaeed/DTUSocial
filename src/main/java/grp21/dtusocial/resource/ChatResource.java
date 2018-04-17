85e0e681168d8170991f6d801069c716f4f649fb
Khurram Saeed Malik <s165162@student.dtu.dk>
2018-04-17 10:56:55 +0200

    Packages added

--- src/main/java/grp21/dtusocial/resource/ChatResource.java
+++ src/main/java/grp21/dtusocial/resource/ChatResource.java
@@ -23,53 +23,5 @@ import javax.ws.rs.core.StreamingOutput;
  */
 @Path("chat")
 public class ChatResource {
-    // Generating JSON data in methods below
    
-    @GET
-    @Secured
-    @Path("2")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getProduct2() {
-        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        Writer writer = new BufferedWriter(new OutputStreamWriter(baos));
-        final javax.json.stream.JsonGenerator gen = Json.createGenerator(writer);
-        gen.writeStartObject()
-                .write("id", 2)
-                .write("name", "Mouse")
-                .write("price", 50.0)
-                .writeEnd();
-        gen.close();
-        StreamingOutput stream = new StreamingOutput() {
-            public void write(OutputStream os) throws IOException {
-                Writer writer = new BufferedWriter(new OutputStreamWriter(os));
-                writer.write(new String(baos.toByteArray()));
-                writer.flush();
-            }
-        };
-        return Response.ok(stream).type(MediaType.APPLICATION_JSON).build();
-    } 
-    @GET
-    @Path("3")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getProduct3() {
-        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        Writer writer = new BufferedWriter(new OutputStreamWriter(baos));
-
-        final JsonObject model = Json.createObjectBuilder()
-                .add("id", 3)
-                .add("name", "Keyboard")
-                .add("price", 180.0)
-                .build();
-        StreamingOutput stream = new StreamingOutput() {
-            @Override
-            public void write(OutputStream os) throws IOException,
-                    WebApplicationException {
-                Writer writer = new BufferedWriter(new OutputStreamWriter(os));
-                writer.write(model.toString());
-                writer.flush();
-            }
-        };
-        return Response.ok(stream).type(MediaType.APPLICATION_JSON).build();
-    }
-
 }
