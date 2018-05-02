cbbdf6b398240caa1a894385bc707b4ae823602f
Swagam <s165155@student.dtu.dk>
2018-05-02 15:43:52 +0200

    Logic added for Group

--- src/main/java/grp21/dtusocial/resource/GroupResource.java
+++ src/main/java/grp21/dtusocial/resource/GroupResource.java
@@ -9,9 +9,13 @@ import brugerautorisation.data.Bruger;
 import grp21.dtusocial.model.Group;
 import grp21.dtusocial.service.GroupService;
 import java.util.List;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  *
@@ -22,9 +26,15 @@ public class GroupResource {
     private GroupService groupService = GroupService.getInstance();
     
     @PUT
-    public void createGroup(Bruger bruger){
-        
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createGroup(Group group){
+        groupService.addGroup(group);
+        return Response.ok().build();
     }
-         
+    @GET
+    public List<Group> getGroups(){
+        return groupService.getGroups();
+    }     
 }
 
